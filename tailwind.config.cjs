7b10dff2c0854808689c8b203923fb1e9194dee7
Jonas Karlsson <jonas@jonaskarlsson.dev>
2023-04-04 22:43:08 +0200

    Move custom colors to extend tailwind defaults instead of overwriting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,14 +3,14 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-    },
-    colors: {
-      'dark-primary': '#222831',
-      'dark-secondary': '#393E46',
-      'dark-contrast': '#EEEEEE',
-      'dark-accent': '#0dab76',
-      'dark-accent-secondary': '#CEB1BE',
-      'dark-accent-secondary-alt': '#dadff7',
+      colors: {
+        'dark-primary': '#222831',
+        'dark-secondary': '#393E46',
+        'dark-contrast': '#EEEEEE',
+        'dark-accent': '#0dab76',
+        'dark-accent-secondary': '#CEB1BE',
+        'dark-accent-secondary-alt': '#dadff7',
+      },
     },
   },
   plugins: [],
